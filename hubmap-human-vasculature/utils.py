31b64bb0ae9661e43963cdb4b37b248123c402e6
arjunchainani <arjun.k.chainani@gmail.com>
2024-02-05 18:00:44 -0500

    cleaned dataset

--- hubmap-human-vasculature/utils.py
+++ hubmap-human-vasculature/utils.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import re
+import itertools
 from PIL import Image
 
 def clean_dataset(dir):
@@ -15,17 +16,18 @@ def clean_dataset(dir):
             image = Image.open(os.path.join(dir, file))
             image.save(os.path.join(dir, file[:-4] + ".png"), "PNG")
             os.remove(os.path.join(dir, file))
+            del image
     
     files = glob.glob1(dir, "*.png")
     files = sorted(files, key=lambda x:float(re.findall("(\d+)",x)[0]))
     print(files)
     
-    for file_num, file in enumerate(files):
+    for file_num, file in list(zip(itertools.count(1, 0.5), files)):
         path = os.path.join(dir, file)
-        os.rename(path, os.path.join(dir, str(file_num) + file[file.index('_'):]))
+        os.rename(path, os.path.join(dir, str(int(file_num)) + file[file.index('_'):]))
     
     print(os.listdir(dir))
 
         
 
-clean_dataset("./test2")
+# clean_dataset("./data/train")
