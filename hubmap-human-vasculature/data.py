5e286f217dc8323ea2aa58761fd6ae4015eaff2e
arjunchainani <arjun.k.chainani@gmail.com>
2024-02-06 22:44:29 -0500

    working on dataset class

--- hubmap-human-vasculature/data.py
+++ hubmap-human-vasculature/data.py
@@ -1,17 +1,37 @@
 import os
 import torch
 import torchvision
+from PIL import Image
 
 class DeepGlobeRoadExtractionDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, transforms=None):
+    def __init__(self, img_dir, transforms=None, target_transforms=None):
         self.img_dir = img_dir
         self.transforms = transforms
+        self.target_transforms = target_transforms
 
     def __len__(self):
-        pass
+        return len([file for file in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, file))])
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, idx, "_sat.jpg")
-        mask_path = os.path.join(self.img_dir, idx, "_mask.png")
-        image = torchvision.io.read_image(img_path)
-        mask = torchvision.io.read_image(mask_path)
+        img_path = os.path.join(self.img_dir, str(idx + 1) + "_sat.png")
+        mask_path = os.path.join(self.img_dir, str(idx + 1) + "_mask.png")
+        image = Image.open(img_path)
+        mask = Image.open(mask_path)
+
+        if self.transforms:
+            image = self.transforms(image)
+        if self.target_transforms:
+            mask = self.target_transforms(mask)
+
+        return image, mask
+    
+def test():
+    dataset = DeepGlobeRoadExtractionDataset('data/train')
+    print(len(dataset))
+    print(dataset[0])
+    # print(dataset[0:5].shape)
+
+if __name__ == '__main__':
+    # test()
+
+    torchvision.transforms.v2()
