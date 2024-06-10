fixed issue with normalizing mask tensors

hubmap-human-vasculature/data.py
@@ -46,18 +46,14 @@ class DeepGlobeRoadExtractionDataset(torch.utils.data.Dataset):
         for index, (image, mask) in enumerate(zip(images, masks)):
             image = torch.from_numpy(np.array(image, dtype=np.float32))
             mask = torch.from_numpy(np.array(mask, dtype=np.float32))
-#             print(mask)
             
             image_min, _ = torch.min(image, dim=-1, keepdim=True)
             image_max, _ = torch.max(image, dim=-1, keepdim=True)
             images[index] = (image - image_min) / (image_max - image_min)
-#             print(image)
             
-            mask_min, _ = torch.min(mask, dim=-1, keepdim=True)
-            mask_max, _ = torch.max(mask, dim=-1, keepdim=True)
-            masks[index] = (mask - mask_min) / (mask_max - mask_min)
+            mask[mask == 255.0] = 1.0
+            masks[index] = mask            
             
-#         print(masks[0])
         images = torch.from_numpy(np.array(images, dtype=np.float32))
         masks = torch.from_numpy(np.array(masks, dtype=np.float32))
 